187348143d55f269b994170e1925ddb5e4985314
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2020-09-08 09:16:24 +0100

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const cors = require('cors')
 const path = require("path");
 require("dotenv").config()
 
@@ -24,6 +25,7 @@ mongoose.connect(
     err => console.log(err)
 )
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, "views")))
